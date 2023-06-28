ef9d0acf87ece56871c96d5fe37c19c101d14e37
KeumBoSeong <137870190+KeumBoSeong@users.noreply.github.com>
2023-06-28 17:07:20 +0900

    Create MyHelloWorld.cs

---
+++ Day 002/MyHelloWorld.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelloWorld
+{
+    internal class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 100;
+            b = 200;
+
+            Console.Write($"{a} {b}");
+            Console.WriteLine();
+            //Console.WriteLine($"{b} {a}");
+
+        }
+    }
+}
