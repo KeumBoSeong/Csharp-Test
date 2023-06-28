caa7d8cd0db2b9f17c454b34bf9ef94d53bc0826
KeumBoSeong <137870190+KeumBoSeong@users.noreply.github.com>
2023-06-28 17:05:56 +0900

    Create APP Quiz1.cs

---
+++ Day 001/APP Quiz1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Quiz._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int 키;
+            키 = int.Parse(Console.ReadLine());
+
+            int result = 키 - 110;
+
+            Console.Write(result);
+
+        }
+    }
+}
