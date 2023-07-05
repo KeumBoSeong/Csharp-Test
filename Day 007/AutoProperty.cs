0696ea3b43847f724db36399609e04903e224b57
KeumBoSeong <137870190+KeumBoSeong@users.noreply.github.com>
2023-07-05 17:30:43 +0900

    Create AutoProperty.cs

---
+++ Day 007/AutoProperty.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProperty
+{
+
+    //P346.
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine(birth.Name);
+            Console.WriteLine(birth.Birthday.ToShortDateString());
+            Console.WriteLine(birth.Age);
+
+            Console.WriteLine();
+
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 06, 28);
+            Console.WriteLine(birth.Name);
+            Console.WriteLine(birth.Birthday.ToShortDateString());
+            Console.WriteLine(birth.Age);
+        }
+    }
+}
