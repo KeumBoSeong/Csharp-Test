6338d32b2b2521e4fb46f38defbdcfdc8325557b
KeumBoSeong <137870190+KeumBoSeong@users.noreply.github.com>
2023-10-23 17:32:17 +0900

    Create Ex4.7.cs

---
+++ OpenCV/Ex4.7.cs
@@ -0,0 +1,41 @@
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "C:\\Temp\\img\\vtest.avi";
+            VideoCapture capture = new VideoCapture(0);
+            capture.Set(VideoCaptureProperties.FrameWidth, 1960);
+            capture.Set(VideoCaptureProperties.FrameHeight, 1080);
+            Mat frame = new Mat();
+
+            while(true)
+            {
+                //if (capture.PosFrames == capture.FrameCount)
+                //    capture.Open(path);
+
+                if(capture.IsOpened() == true)
+                {
+                    capture.Read(frame);
+                    Cv2.ImShow("VideoFrame", frame);
+
+                    if (Cv2.WaitKey(33) == 'q')
+                        break;
+                }
+
+
+            }
+
+            capture.Release();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
