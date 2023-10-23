045853d942eb8a3a8f33393e9d27aab9eb0169d6
KeumBoSeong <137870190+KeumBoSeong@users.noreply.github.com>
2023-10-23 17:33:13 +0900

    Create MatrixArray.cs

---
+++ OpenCV/MatrixArray.cs
@@ -0,0 +1,26 @@
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[9];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i + 1;
+            }
+
+            Mat m1 = new Mat(3, 3, MatType.CV_32S, data);
+
+            Console.WriteLine(m1.Dump());
+
+        }
+    }
+}
